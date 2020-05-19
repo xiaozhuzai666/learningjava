3b82619955de0d746b082ee5e1062b1b6820ff03
xiaozhuzai666 <2833910885@qq.com>
2020-05-19 14:18:11 +0800

    this is my java project.

--- Shape1.java
+++ Shape1.java
@@ -1,62 +1,72 @@
-interface Shape1 {
-    final static double PI = 3.1415926;
-    double area();
+import java.lang.Math;
+interface Shape1{
+    abstract double Area();
 }
-class Circle implements Shape1 {
-    double r;
-    public Circle(double r) {
-        this.r = r;
+class Rectangle implements Shape1{
+    private double a;
+    Rectangle(){
     }
-    public double area() {
-        return Shape1.PI* r * r;
+    Rectangle(double a){
+        this.a=a;
+    }
+    public double getX(){
+        return a;
+    }
+    public double Area(){
+        return getX()*getX();
     }
 }
+class Circle implements Shape1{
+    private double r;
+    Circle(){
 
-class Rectangle implements Shape1 {
-    double a;
-    double b;
-    public Rectangle(double a, double b) {
-        this.a = a;
-        this.b = b;
     }
-    public double area() {
-        return a * b;
+    Circle(double r){
+        this.r=r;
+    }
+    public double getR(){
+        return r;
     }
 
+    public double Area(){
+        return 3.14*getR()*getR();
+    }
 }
-class Star implements Shape1 {
-    public double area() {
+class Star implements Shape1{
+    Star(){}
+    public double Area(){
         return 0;
     }
-    public static boolean isStar(Shape1 s) {
-        return (s instanceof Star) ? true : false;
+    public boolean isStar(){
+        if(Area()==0)
+            return true;
+        else
+            return false;
     }
 }
-
-class ShapeForInterface {
-    public static void main(String[] args) {
-        Circle c = new Circle(5);
-
-        Rectangle l = new Rectangle(3, 4);
-
-        Star star = new Star();
-
-        Shape1[] array = {(Shape1) c, star, star, (Shape1) l, star, star};
-
-        for (int i = 0, j = 1; i < array.length; i++)//此处可进行改进，提高效率
-
-            {
-
-                if (Star.isStar(array[i]))
-
-                    System.out.println("is  Star:" + j++);
-
-                else
-
-                    System.out.println("not Star area:" + array[i].area());
-
+class Vector implements Shape1{
+    public double Area(){
+        return 0;
+    }
+}
+class Test6 {
+    public static void main(String args[]){
+        Shape1[] s=new Vector[9];//新建对象数组
+        int n;
+        for(int i=0;i<s.length;i++){
+            n=(int)(Math.random()*5);//随机函数产生随机值
+            switch(n){
+                case 1:s[i]=new Circle();break;//父类引用指向子类对象
+                case 2:s[i]=new Rectangle();break;
+                case 3:s[i]=new Star();break;
             }
+        }
+        for(int i=0;i<s.length;i++){
+            if(s[i].Area()==0){
+                System.out.println(s[i]+"是个Star");
+            }
+            else System.out.println(s[i].Area());
+        }
 
     }
-
 }
